9e0dff430705c1c7f29f6f3828097ce8c1c75c0b
Alec Krafzik <alec.krafzik@grover.com>
2022-06-20 13:55:30 -0300

    added base data to find

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { ApolloServer, gql } = require('apollo-server');
+const employees = require("@jsdevtools/static-mock-data/employees.json");
 
 const typeDefs = gql`
     type Query {
@@ -6,15 +7,23 @@ const typeDefs = gql`
     }
     type Employee {
         id: ID!
-        firstName: String,
-        lastName: String,
-        designation: String,
-        department: String,
-        nearestCity: String
+        username: String,
+        gender: String,
+        email: String,
+        department: String @deprecated (reason: "Because I want to test a deprecation decorator"),
+        roles: [String]
     }
 `
 
-const gqlServer = new ApolloServer({ typeDefs });
+const resolvers = {
+    Query: {
+        employees: () => {
+            return employees
+        }
+    }
+}
+
+const gqlServer = new ApolloServer({ typeDefs, resolvers });
 
 gqlServer.listen({ port: process.env.port || 4000 })
     .then(({ url }) => console.log(`graphQL server started on ${url}`))
